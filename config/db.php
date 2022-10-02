bf357ad4ea281d1262bf8a7ae7b7f8b1af489e77
benaiah-ke <benaiahpraise@gmail.com>
2022-10-02 13:37:23 +0300

    adjust live db connection credentials

--- config/db.php
+++ config/db.php
@@ -4,8 +4,8 @@ ini_set('display_errors', 1); error_reporting(E_ALL);
 
 define('DB_HOST', 'localhost');
 define('DB_DATABASE', 'ehouseco_sendglobal');
-define('DB_USER', 'ehouseco@localhost');
-define('DB_PASSWORD', '');
+define('DB_USER', 'ehouseco_benaiah');
+define('DB_PASSWORD', '.praise30');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
