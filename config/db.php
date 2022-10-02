e361819d426df5a016d1ce144645610d8208496e
benaiah-ke <benaiahpraise@gmail.com>
2022-10-02 13:27:52 +0300

    dp config for deployment

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', '127.0.0.1');
-define('DB_DATABASE', 'sendglobal');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '.praise30');
+define('DB_HOST', 'localhost');
+define('DB_DATABASE', 'ehouseco_sendglobal');
+define('DB_USER', 'ehouseco@localhost');
+define('DB_PASSWORD', '');
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
 
